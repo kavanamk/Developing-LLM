1da2756724695712731d2444c5bc8ed2cbe629ca
Kavana Manvi Krishnamurthy <kavana.manvi@gmail.com>
2025-10-02 09:57:07 -0500

    Create  ReLu-Conv-BN.py

---
+++ GAN/ReLu-Conv-BN.py
@@ -0,0 +1,11 @@
+class ReLUConvBN(nn.Module):
+    def __init__(self, C_in, C_out, kernel_size, stride, padding, affine=True):
+        super(ReLUConvBN, self).__init__()
+        self.op = nn.Sequential(
+            nn.ReLU(inplace=False),
+            nn.Conv2d(C_in, C_out, kernel_size, stride=stride, padding=padding, bias=False),
+            nn.BatchNorm2d(C_out, affine=affine)
+        )
+
+    def forward(self, x):
+        return self.op(x)
